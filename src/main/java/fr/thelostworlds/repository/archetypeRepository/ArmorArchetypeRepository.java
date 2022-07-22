252c8f2614eb8078402618ea063ed011f2e57a5e
Neo <neobabylone@gmail.com>
2022-07-23 00:49:21 +0200

    ArchetypeRepository

--- src/main/java/fr/thelostworlds/repository/archetypeRepository/ArmorArchetypeRepository.java
+++ src/main/java/fr/thelostworlds/repository/archetypeRepository/ArmorArchetypeRepository.java
@@ -1,4 +1,10 @@
 package fr.thelostworlds.repository.archetypeRepository;
 
+import fr.thelostworlds.entity.archetype.armorArchetype.ArmorArchetype;
+
+import java.util.HashMap;
+
 public class ArmorArchetypeRepository {
+
+    public static HashMap<String, ArmorArchetype> armorArchetypeHashMap;
 }
--- src/main/java/fr/thelostworlds/repository/archetypeRepository/WeaponArchetypeRepository.java
+++ src/main/java/fr/thelostworlds/repository/archetypeRepository/WeaponArchetypeRepository.java
@@ -1,4 +1,10 @@
 package fr.thelostworlds.repository.archetypeRepository;
 
+import fr.thelostworlds.entity.archetype.weaponArchetype.WeaponArchetype;
+
+import java.util.HashMap;
+
 public class WeaponArchetypeRepository {
+
+    public static HashMap<String, WeaponArchetype> weaponArchetypeHashMap;
 }
